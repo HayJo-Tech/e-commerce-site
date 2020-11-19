bb53db5fdf7a831a6d8d0311fba25d06ffaf7b08
John Pendergrass <johnpendergrass@Johns-MacBook.local>
2020-11-18 21:15:59 -0600

    wrote code for base of navbar using react-bootstrap and Link from react-router-dom

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,33 @@
 import React, { Component } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import { Link } from "react-router-dom";
+import logo from "../assets/icons/iconfinder_Credit_Card_2921795.png";
 
-class Navbar extends Component {
+class MyNavbar extends Component {
   render() {
     return (
-      <div>
-        <h3>Hello from Navbar</h3>
-      </div>
+      <Navbar bg="dark" variant="dark">
+        <Link to="/">
+          {/* //   https://www.iconfinder.com/icons/2921795/banking_business_credit_card_e-commerce_payment_purchase_transaction_icon */}
+          <img src={logo} alt="store" className="navbar-brand"></img>
+        </Link>
+        <Navbar.Brand href="#home">HayJo Shop</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Link to="/" className="nav-link">
+            products
+          </Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-info">Search</Button>
+        </Form>
+      </Navbar>
     );
   }
 }
 
-export default Navbar;
+export default MyNavbar;
